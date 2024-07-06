005c30207d00f760ba30a053fb99cc31df72f169
mdco-ld <migueldco14@gmail.com>
2024-07-06 20:03:10 -0300

    remove unecessary comments

--- src/wb/serialization.cpp
+++ src/wb/serialization.cpp
@@ -131,59 +131,23 @@ void deserializeHeader(std::istream &in, int &version) {
     if (header[0] != 'W' || header[1] != 'B') {
         throw std::runtime_error("Invalid magic bytes for deserialization");
     }
-	deserializeV1(in, version);
+    deserializeV1(in, version);
 }
 
 void serialize(std::ostream &out, std::vector<wb::Drawing> &drawings) {
-    // out << drawings.size() << "\n";
-    // for (wb::Drawing &drawing : drawings) {
-    //     if (drawing.getPoints().size() > 0) {
-    //         out << 1 << "\n";
-    //         out << drawing.getPoints().front().x << " "
-    //             << drawing.getPoints().front().y << "\n";
-    //         continue;
-    //     }
-    //     out << drawing.getLineSegments().size() + 1 << "\n";
-    //     out << drawing.getLineSegments().front().start.x << " "
-    //         << drawing.getLineSegments().front().start.y << " ";
-    //     for (geometry::LineSegment line : drawing.getLineSegments()) {
-    //         out << line.end.x << " " << line.end.y << " ";
-    //     }
-    //     out << "\n";
-    // }
     serializeHeader(out, 1);
     serializeV1(out, drawings);
 }
 
 void deserialize(std::istream &in, std::vector<wb::Drawing> &output) {
-    // if (!output.empty()) {
-    //     output.clear();
-    // }
-    // int numDrawings;
-    // in >> numDrawings;
-    // if (numDrawings < 0) {
-    //     throw std::runtime_error("Invalid number of drawings");
-    // }
-    // for (int _ = 0; _ < numDrawings; _++) {
-    //     std::vector<geometry::Vec2> points;
-    //     int numPoints;
-    //     in >> numPoints;
-    //     for (int i = 0; i < numPoints; i++) {
-    //         int x;
-    //         int y;
-    //         in >> x >> y;
-    //         points.push_back(geometry::Vec2{x, y});
-    //     }
-    //     output.push_back(wb::Drawing::fromPoints(points));
-    // }
-	int version;
-	deserializeHeader(in, version);
-	if (version == 1) {
-		deserializeV1(in, output);
-	} else {
-		throw std::runtime_error("Unsupported format version");
-	}
-	PRINT_DBG(output.size());
+    int version;
+    deserializeHeader(in, version);
+    if (version == 1) {
+        deserializeV1(in, output);
+    } else {
+        throw std::runtime_error("Unsupported format version");
+    }
+    PRINT_DBG(output.size());
 }
 
 }; // namespace serialization
